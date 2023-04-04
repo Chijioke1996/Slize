56cf15ff0877e98e04c2d6293b2bb07722885269
Chijioke1996 <111643913+Chijioke1996@users.noreply.github.com>
2023-04-04 16:09:25 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector(".menu")
+const boxy = document.querySelector(".boxy")
+// const close = document.querySelector(".close-btn")
+
+menu.addEventListener("click", () => {
+    boxy.classList.add("active")
+   
+})
+
