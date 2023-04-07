a60e35b44c5f4ca7c93a1151b9dceb632c39b50f
Chijioke1996 <111643913+Chijioke1996@users.noreply.github.com>
2023-04-07 06:00:15 +0100

    first3 commit

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const menu = document.querySelector(".menu")
 const wrapper = document.querySelector(".wrapper")
-// const close = document.querySelector(".close-btn")
+const closeBtn = document.querySelector(".close-btn")
+const listItems = document.querySelectorAll(".list")
 
 menu.addEventListener("click", () => {
     wrapper.classList.add("active")
    
 })
+
+closeBtn.addEventListener("click", () => {
+    wrapper.classList.remove("active")
+   
+})
+
+listItems.forEach(item => {
+    item.addEventListener("click", () => {
+        wrapper.classList.remove("active")
+    })
+})
+
+
+
 
